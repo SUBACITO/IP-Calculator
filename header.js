1c7f402622556172177d8b67abe659b2bb1611e2
SUBACITO <subacitodev@gmail.com>
2024-11-12 13:57:50 +0700

    add icon and home page

--- header.js
+++ header.js
@@ -6,8 +6,24 @@ function displaySidebar() {
             <div class="sidebar-content">
                 <nav>
                     <ul>
-                        <li><a href="../VLSM/index.html">VLSM</a></li>
-                        <li><a href="../IP/index.html">IP</a></li>
+                        <li><a href="/VLSM">
+                              <div class="span3">
+                                <i class="demo-icon icon-sort-numeric-outline" title="VLSM">&#xe800;</i> <span class="i-name"></span><span class="i-code"></span>
+                                </div>
+                            </a>
+                        </a></li>
+                        <li><a href="/"><div class="span3"">
+                            <i class="demo-icon icon-home-circled" title="Home">&#xe801;</i> <span class="i-name"></span><span class="i-code"></span>
+                            </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/IP">
+                                <div class="span3"">
+                                    <i class="demo-icon icon-calc" title="IP">&#xf1ec;</i> <span class="i-name"></span><span class="i-code"></span>
+                                </div>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
